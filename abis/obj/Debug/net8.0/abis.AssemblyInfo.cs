6a19a19a4f6cf706bbed243258916e5274d4342f
TeoTeich <katiasarmanova@gmail.com>
2024-11-12 13:56:19 +0300

    Были добавлены стили

--- abis/obj/Debug/net8.0/abis.AssemblyInfo.cs
+++ abis/obj/Debug/net8.0/abis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("abis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2894ef2f4f846974b94f4697def021f26fa5712d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("abis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("abis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
